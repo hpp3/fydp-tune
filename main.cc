bc773cb1139ee1b5523dda0c15046302695fb3ba
Eddy Gao <Eddy Gao>
2017-03-25 00:01:11 -0400

    updated cpack macros and performed tests

--- main.cc
+++ main.cc
@@ -1,113 +1,192 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <utility>
-#include <algorithm>
 
 #include "cltune.h"
-#include "packed_array_impl.h"
-#include "json11.hpp"
 #include "cpack.h"
-
+#include "packed_array_impl.h"
 using namespace std;
-void packed_sum(int bitsize) {
-    const auto simpleSize = int32_t{1024};
-    PackedArrayImpl a("test", bitsize, simpleSize);
 
-    for(int i=0; i<10; i++) {
-        a.set(i, 3);
+void tile() {
+    uint32_t dim = 4;
+    uint32_t M = dim;
+    uint32_t N = dim;
+    uint32_t K = dim;
+    uint32_t group_size = 2;
+
+    PackedArrayImpl a("arrA", 4, M*K);
+    PackedArrayImpl b("arrB", 4, K*N);
+
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < K; j++) {
+            a.set(i*M + j, (i*M + j) % 16);
+        }
+    }
+    
+    for (size_t i = 0; i < K; i++) {
+        for (size_t j = 0; j < N; j++) {
+            b.set(i*M + j, (i*K + j) % 16);
+        }
     }
 
-    int group_size = 32;
-
-    cout<<"size: "<<a.size()<<endl;
-    cout<<"physical size: "<<a.physical_size()<<endl;
-    cout<<"capacity: "<<a.capacity()<<endl;
-    cout<<"physical capacity: "<<a.physical_capacity()<<endl;
-
-    // read the file
-    ifstream infile{ "../samples/fydp-tune/packed_sum.cl" };
+    ifstream infile{ "../samples/fydp-tune/multi_tile.cl" };
     string kernel{ istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
-    string header = a.getConfig().generateOpenCLCode(false, group_size);
-    string full_kernel = header + kernel;
+    string headerA = a.getConfig().generateOpenCLCode(true, group_size);
+    string headerB = b.getConfig().generateOpenCLCode(true, group_size);
+    string full_kernel = headerA + headerB + kernel;
 
+    cout<<full_kernel<<endl;
 
-    // Creates data structures
-    int32_t *cells = a.getCells();
-    std::vector<int32_t> arr(cells, cells+a.physical_capacity());
-    std::vector<int32_t> sum(1);
-    sum[0] = 666;
+    int32_t *cellA = a.getCells();
+    int32_t *cellB = b.getCells();
+    std::vector<int32_t> arrA(cellA, cellA+a.physical_capacity());
+    std::vector<int32_t> arrB(cellB, cellB+b.physical_capacity());
+    std::vector<int32_t> target(M*N);
+    std::vector<int32_t> arrC(M*N);
 
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            target[i*N + j] = 0;
+            for (size_t k = 0; k < K; k++) {
+                target[i*N+ j] += a.get(i*K+k) * b.get(k*N+j); 
+            }
+            cout << target[i*N + j] << " ";
+        }
+        cout << endl;
+    }
 
-    // Initializes the tuner (platform 0, device 0)
     cltune::Tuner tuner(size_t{0}, size_t{0});
+    auto id = tuner.AddKernelFromString(full_kernel, "multi_tile", {M, N}, {group_size, group_size});
+    tuner.SetReference({"../samples/fydp-tune/ref.cl"}, "ref", {M, N}, {group_size, group_size});
 
-    // Adds a kernel which supports unrolling through the UNROLL parameter. Note that the kernel
-    // itself needs to implement the UNROLL parameter and (in this case) only accepts a limited
-    // amount of values.
-    auto id = tuner.AddKernelFromString(full_kernel, "packed_sum", {(unsigned long)a.physical_capacity()}, {(unsigned long)group_size});
+    tuner.AddArgumentScalar((int)M);
+    tuner.AddArgumentScalar((int)N);
+    tuner.AddArgumentScalar((int)K);
+    tuner.AddArgumentInput(arrA);
+    tuner.AddArgumentInput(arrB);
+    tuner.AddArgumentInput(target);
+    tuner.AddArgumentOutput(arrC);
+    tuner.SetNumRuns(1);
 
-    // Sets the function's arguments. Note that all kernels have to accept (but not necessarily use)
-    // all input arguments.
-    tuner.AddArgumentInput(arr);
-    tuner.AddArgumentOutput(sum);
 
     // Starts the tuner
     tuner.Tune();
 
-    // Prints the results to screen
-    tuner.PrintToScreen();
-    vector<pair<string,string>> descriptions;
-    descriptions.push_back(make_pair("packingParam", "2"));
-
-    tuner.PrintJSON("fydp-tune.json", descriptions);
 }
 
-double reduce(int bitsize) {
-    int group_size = 32;
-    const auto simpleSize = int32_t{1 << 26};
-    PackedArrayImpl a("test", bitsize, simpleSize);
-    for (size_t i = 0; i < simpleSize; i++) {
-        a.set(i, i % 4);
+void easy_tile() {
+    TUNE_START(vector<int>({4, 8, 16, 32}), vector<int>({8,16,32}), vector<bool>({true, false}));
+    //TUNE_START(vector<int>({4}), vector<int>({8}), vector<bool>({true}));
+    uint32_t dim = 1024;
+    uint32_t M = dim;
+    uint32_t N = dim;
+    uint32_t K = dim;
+
+
+    vector<int32_t> a;
+    vector<int32_t> b;
+    std::vector<int32_t> target(M*N);
+    std::vector<int32_t> arrC(M*N);
+
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < K; j++) {
+            a.push_back((i*M + j) % 16);
+        }
     }
-    cout<<"bitsize:"<<bitsize<<endl;
-    cout<<"size: "<<a.size()<<endl;
-    cout<<"physical size: "<<a.physical_size()<<endl;
-    cout<<"capacity: "<<a.capacity()<<endl;
-    cout<<"physical capacity: "<<a.physical_capacity()<<endl;
+    
+    for (size_t i = 0; i < K; i++) {
+        for (size_t j = 0; j < N; j++) {
+            b.push_back((i*K + j) % 16);
+        }
+    }
 
-    ifstream infile{ "../samples/fydp-tune/reduce.opencl" };
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            target[i*N + j] = 0;
+            for (size_t k = 0; k < K; k++) {
+                target[i*N+ j] += a[i*K+k] * b[k*N+j]; 
+            }
+        }
+    }
+
+    PROCESS_VEC(a, "arrA");
+    PROCESS_VEC(b, "arrB");
+
+    std::string kernel = KERNEL_STRING("../samples/fydp-tune/multi_tile.cl");
+    cltune::Tuner tuner(size_t{0}, size_t{0});
+    auto id = tuner.AddKernelFromString(kernel, "multi_tile", {M, N}, {cpack_groupsize, cpack_groupsize});
+    //tuner.SetReference({"../samples/fydp-tune/ref.cl"}, "ref", {M, N}, {cpack_groupsize, cpack_groupsize});
+
+    tuner.AddArgumentScalar((int)M);
+    tuner.AddArgumentScalar((int)N);
+    tuner.AddArgumentScalar((int)K);
+    tuner.AddArgumentInput(a);
+    tuner.AddArgumentInput(b);
+    tuner.AddArgumentInput(target);
+    tuner.AddArgumentOutput(arrC);
+    tuner.SetNumRuns(100);
+
+    tuner.Tune();
+    TUNE_END(tuner);
+}
+
+void mult() {
+    uint32_t gg = 1024;
+    uint32_t M = gg;
+    uint32_t N = gg;
+    uint32_t K = gg;
+    uint32_t group_size = 32;
+
+    PackedArrayImpl a("arrA", 32, M*K);
+    PackedArrayImpl b("arrB", 32, K*N);
+
+
+    ifstream infile{ "../samples/fydp-tune/matrix.cl" };
     string kernel{ istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
-    string header = a.getConfig().generateOpenCLCode(false, group_size);
-    string full_kernel = header + kernel;
+    string headerA = a.getConfig().generateOpenCLCode(false, group_size);
+    string headerB = b.getConfig().generateOpenCLCode(false, group_size);
+    string full_kernel = headerA + headerB + kernel;
 
-    int32_t *cells = a.getCells();
+    cout<<full_kernel<<endl;
 
-    std::vector<int32_t> data(cells, cells+a.physical_capacity());
-    std::vector<int32_t> sum(1);
-    cout << data.size() << " " << data.capacity() << endl;
+    int32_t *cellA = a.getCells();
+    int32_t *cellB = b.getCells();
+    std::vector<int32_t> arrA(cellA, cellA+a.physical_capacity());
+    std::vector<int32_t> arrB(cellB, cellB+b.physical_capacity());
+    std::vector<int32_t> target(M*N);
+    std::vector<int32_t> arrC(M*N);
 
-    cltune::Tuner tuner(size_t{1}, size_t{0});
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            target[i*N + j] = 0;
+            for (size_t k = 0; k < K; k++) {
+                target[i*N+ j] += a.get(i*K+k) * b.get(k*N+j); 
+            }
+        }
+    }
 
-    auto id = tuner.AddKernelFromString(full_kernel, "reduction_sum_scalar_complete2", {(unsigned long)simpleSize}, {(unsigned long)group_size});
+    cltune::Tuner tuner(size_t{0}, size_t{0});
+    auto id = tuner.AddKernelFromString(full_kernel, "matrixMult", {M, N}, {group_size, group_size});
+    tuner.SetReference({"../samples/fydp-tune/ref.cl"}, "ref", {M, N}, {group_size, group_size});
 
-    tuner.AddArgumentInput(data);
-    tuner.AddArgumentInput(sum);
+    tuner.AddArgumentScalar((int)M);
+    tuner.AddArgumentScalar((int)N);
+    tuner.AddArgumentScalar((int)K);
+    tuner.AddArgumentInput(arrA);
+    tuner.AddArgumentInput(arrB);
+    tuner.AddArgumentInput(target);
+    tuner.AddArgumentOutput(arrC);
+    tuner.SetNumRuns(100);
 
-    tuner.SetNumRuns(1000);
+
+    // Starts the tuner
     tuner.Tune();
 
-    tuner.PrintJSON("fydp-tune.json", {});
-    ifstream jstream{ "fydp-tune.json" };
-    string test_output{ istreambuf_iterator<char>(jstream), istreambuf_iterator<char>() };
-    string error;
-    json11::Json json = json11::Json::parse(test_output, error, json11::JsonParse::STANDARD);
-    return json["results"][0]["time"].number_value();
 }
 
 void easy_reduce() {
-    TUNE_START(vector<int>({2, 4, 8, 16, 32}));
-    int group_size = 32;
+    //TUNE_START(vector<int>({2, 4, 8, 16, 32}));
+    TUNE_START(vector<int>({2, 4, 8, 16, 32}), vector<int>({8,16,32}), vector<bool>({false}));
     const auto simpleSize = int32_t{1 << 26};
 
     std::vector<int32_t> data;
@@ -116,13 +195,13 @@ void easy_reduce() {
     for (size_t i = 0; i < simpleSize; i++) {
         data.push_back(rand() % 4);
     }
-    PROCESS_VEC(data, "test", false, group_size);
+    PROCESS_VEC(data, "test");
 
     std::string kernel = KERNEL_STRING("../samples/fydp-tune/reduce.opencl");
 
-    cltune::Tuner tuner(size_t{1}, size_t{0});
+    cltune::Tuner tuner(size_t{0}, size_t{0});
 
-    auto id = tuner.AddKernelFromString(kernel, "reduction_sum_scalar_complete2", {(unsigned long)simpleSize}, {(unsigned long)group_size});
+    auto id = tuner.AddKernelFromString(kernel, "reduction_sum_scalar_complete2", {(unsigned long)simpleSize}, {(unsigned long)cpack_groupsize});
 
     tuner.AddArgumentInput(data);
     tuner.AddArgumentInput(sum);
@@ -132,21 +211,10 @@ void easy_reduce() {
     TUNE_END(tuner);
 }
 
-
 int main() {
-    vector<int> v = {2, 4, 8, 16, 32};
-    vector<pair<int,double>> times;
-    for (size_t i = 0; i < v.size(); i++) {
-        //packed_sum(v[i]);
-        times.push_back(make_pair(v[i], reduce(v[i])));
-    }
-    sort(times.begin(), times.end(), [](const pair<int, double>& a, const pair<int, double>& b) -> bool { 
-        return a.second < b.second; 
-    });
 
-    cout << "Summary:\nBitsize\tTime in ms" << endl;
-    for (size_t i = 0; i < times.size(); i++) {
-        cout << times[i].first << "\t" << times[i].second << endl;
-    }
+    //easy_tile();
+    //tile();
+    easy_reduce();
     return 0;
 }
